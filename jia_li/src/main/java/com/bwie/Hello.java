9332856ccda5f0b4aede8f2870661180e371a841
jlb <jlb@jlb>
2018-03-20 12:34:36 +0800

    提交

---
+++ jia_li/src/main/java/com/bwie/Hello.java
@@ -0,0 +1,12 @@
+package com.bwie;
+
+import java.util.HashMap;
+
+public class Hello {
+	//hashmap底层是基于数组结构，数组的每一项又是有链表组成，当新建一个hashmap时就会初始化数组。
+	public static void main(String [] args){
+		HashMap<String,Object> map = new HashMap<String,Object>();
+		
+	}
+
+}
